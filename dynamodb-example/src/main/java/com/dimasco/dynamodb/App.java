30736a642e569c70282f282517703f5988ee6e01
dimas <yermolovich1987@gmail.com>
2016-10-25 23:35:51 +0300

    Initial commit with POm and some classes.

---
+++ dynamodb-example/src/main/java/com/dimasco/dynamodb/App.java
@@ -0,0 +1,13 @@
+package com.dimasco.dynamodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
